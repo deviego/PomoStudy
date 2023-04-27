f553f32b6fbf9d60a7e54bf4503be65b799e8879
deviego <deviego4@gmail.com>
2023-04-26 21:58:18 -0300

    validate

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,13 +9,46 @@ import {
   TaskInput,
 } from './style'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { z } from 'zod'
+
+import { useForm } from 'react-hook-form'
+
+const newCycleFormSchema = z.object({
+  task: z.string().min(1, 'informe a tarefa'),
+  minutesAmount: z.number().min(5).max(60),
+})
+
+type NewCycleformDAta = z.infer<typeof newCycleFormSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleformDAta>({
+    resolver: zodResolver(newCycleFormSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleSubmitButton(data: any) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisable = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(handleSubmitButton)}>
         <FormContainer>
           <label htmlFor=""> vou trabalhar em </label>
-          <TaskInput id="text" placeholder="Dê um nome para sua tarefa" />
+          <TaskInput
+            id="text"
+            placeholder="Dê um nome para sua tarefa"
+            {...register('task')}
+          />
           <label htmlFor="">durante</label>
           <MinutesAmountInput
             placeholder="00"
@@ -24,6 +57,7 @@ export function Home() {
             min={5}
             max={60}
             id="minutosAmount"
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -36,7 +70,7 @@ export function Home() {
           <span>0</span>
         </CountContainer>
 
-        <StartCountdowButton type="submit" disabled>
+        <StartCountdowButton type="submit" disabled={isSubmitDisable}>
           <Play size={24} />
           Começar
         </StartCountdowButton>
